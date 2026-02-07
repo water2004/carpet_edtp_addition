9a6c627e2b17cd8bed24290cb280e5bfff0213e9
EasterGhost <eg051048@mail.ustc.edu.cn>
2026-02-07 17:44:52 +0800

    Add a custom trade registration feature, allowing tool merchants to sell enchanted tools

--- src/main/java/org/edtp/carpet_edtp_addition/CarpetEdtpAdditionExtension.java
+++ src/main/java/org/edtp/carpet_edtp_addition/CarpetEdtpAdditionExtension.java
@@ -2,7 +2,14 @@ package org.edtp.carpet_edtp_addition;
 
 import carpet.CarpetExtension;
 import net.fabricmc.loader.api.FabricLoader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
+import net.minecraft.item.Items;
+import net.minecraft.village.TradeOffers;
+import net.minecraft.village.VillagerProfession;
+import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
 import org.edtp.carpet_edtp_addition.util.Translation;
 
 public class CarpetEdtpAdditionExtension implements CarpetExtension {
@@ -13,6 +20,26 @@ public class CarpetEdtpAdditionExtension implements CarpetExtension {
     public void onGameStarted() {
         // 注册设置管理器
         CarpetEdtpAdditionSettings.register();
+        registerCustomTrades();
+    }
+
+    private void registerCustomTrades() {
+        Int2ObjectMap<TradeOffers.Factory[]> levelMap = TradeOffers.PROFESSION_TO_LEVELED_TRADE.get(VillagerProfession.TOOLSMITH);
+        if (levelMap != null) {
+            TradeOffers.Factory[] masterTrades = levelMap.get(5);
+            if (masterTrades != null) {
+                List<TradeOffers.Factory> factories = new ArrayList<>(Arrays.asList(masterTrades));
+                factories.add((transaction, entity, random) -> {
+                    if (CarpetEdtpAdditionSettings.villagerMaxEnchantLevel.value() >= 1) {
+                         // SellEnchantedToolFactory(Item item, int basePrice, int maxUses, int experience, float multiplier)
+                         return new TradeOffers.SellEnchantedToolFactory(Items.DIAMOND_HOE, 13, 3, 30, 0.2f)
+                                .create(transaction, entity, random);
+                    }
+                    return null;
+                });
+                levelMap.put(5, factories.toArray(new TradeOffers.Factory[0]));
+            }
+        }
     }
 
     @Override
