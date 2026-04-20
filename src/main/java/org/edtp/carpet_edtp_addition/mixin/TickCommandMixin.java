f3de66a5042164e2ea6589fdd6adb5b76ed8c82f
GBwater <gbwater@outlook.com>
2026-04-20 19:14:36 +0800

    fix: tickCommandForAll rule not effective

--- src/main/java/org/edtp/carpet_edtp_addition/mixin/TickCommandMixin.java
+++ src/main/java/org/edtp/carpet_edtp_addition/mixin/TickCommandMixin.java
@@ -3,24 +3,26 @@ package org.edtp.carpet_edtp_addition.mixin;
 import org.edtp.carpet_edtp_addition.CarpetEdtpAdditionSettings;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 import java.util.function.Predicate;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.server.commands.TickCommand;
+import com.mojang.brigadier.builder.ArgumentBuilder;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(TickCommand.class)
 public class TickCommandMixin {
     
-    @ModifyArg(
+    @Redirect(
         method = "register",
         at = @At(
             value = "INVOKE",
             target = "Lcom/mojang/brigadier/builder/LiteralArgumentBuilder;requires(Ljava/util/function/Predicate;)Lcom/mojang/brigadier/builder/ArgumentBuilder;",
-            ordinal = 0
-        )
+            remap = false
+        ),
+        require = 1
     )
-    private static Predicate<CommandSourceStack> modifyTickCommandPermission(Predicate<CommandSourceStack> original) {
-        return source -> CarpetEdtpAdditionSettings.tickCommandForAll.value() || original.test(source);
+    private static ArgumentBuilder<CommandSourceStack, ?> redirectTickCommandPermission(com.mojang.brigadier.builder.LiteralArgumentBuilder<CommandSourceStack> builder, Predicate<CommandSourceStack> originalRequirement) {
+        return builder.requires(source -> CarpetEdtpAdditionSettings.tickCommandForAll.value() || originalRequirement.test(source));
     }
 }
